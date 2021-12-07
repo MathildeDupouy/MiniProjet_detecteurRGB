84d8a59e95d7242a665c1ab2cf89eb7a33234751
Mathilde <mathilde.dupouy@yahoo.fr>
2021-12-07 12:03:04 +0100

    Affichage des trois couleurs (sur 16 bits)

--- src/detecteurRGB.c
+++ src/detecteurRGB.c
@@ -3,15 +3,13 @@
 #include "LPC8xx.h"
 #include "syscon.h"
 #include "lib_ENS_II1_lcd.h"
+#include "i2c.h"
 #include "rom_api.h"
-#include "ctimer.h"
-#include "uart.h"
 #include "utilities.h"
 #include "swm.h"
 #include "chip_setup.h"
 
-void affichage(char* texte_l1, char* texte_l2);
-float choix_note(char car);
+#include "TCS34725.h"
 
 
 #define BP1 LPC_GPIO_PORT->B0[13]
@@ -24,140 +22,104 @@ float choix_note(char car);
 #define enfonce 0
 #define relache 1
 
-int main(void) {
-	uint8_t menu = 3;
-	uint8_t menu_prec = menu;
+uint8_t erreur_demande = 0;
 
-	//Variables des etats des boutons
-	uint8_t bp1 = BP1;
-	uint8_t bp1_prec = BP1;
-	uint8_t bp2 = BP2;
-	uint8_t bp2_prec = BP2;
-	//Variable pour jouer les notes
-	char car_usart;
-	float freq;
+/**
+ * Le type est par défaut : Repeated byte protocol transaction
+ * reg_ad : adresse du registre que l'on veut appeler, sur 5bits
+ * valeur : valeur a ecrire dans le registre
+ */
+void TCS_write_reg(uint8_t reg_ad,uint8_t valeur){
 
-	//Réglage de la fréquence de l'horloge
-	LPC_PWRD_API->set_fro_frequency(30000);
+	uint8_t I2CMasterBuffer[3]; // ad, #reg, valeur
+	uint8_t I2CWriteLength=2;
+	I2CMasterBuffer[0]=TCS34725_ADDRESS;
 
-	// Activation du périphérique d'entrées/sorties TOR, de la switchmatrix,
-	// du UART, du timer ctimer0, du multi-rate timer
-	LPC_SYSCON->SYSAHBCLKCTRL0 |= GPIO0 | SWM | UART0 | CTIMER0 | MRT;
+	//Command code
+	I2CMasterBuffer[1]=(0b100 << 5) | reg_ad; //CMD 1, TYPE 00, ADDR = 0b100AD
+	//ADDRID = 0b1001 0010 = 92 (AD 0x12)
+	//ADDRRed = 0b1001 0110 = 96 (AD 0x16)
+	I2CMasterBuffer[2]=valeur;
+	I2CmasterWrite(I2CMasterBuffer, I2CWriteLength );
+}
 
-	//Configuration en sortie de la broche 19
-	LPC_GPIO_PORT->DIR0 |= (1 << 19);
-
-	//Configuration de la liaison serie
-		//Horloge de la liaison serie
-		 LPC_SYSCON->UART0CLKSEL = FCLKSEL_FRO_CLK;
-
-		 //Assignation des pins T et R
-		 ConfigSWM(U0_TXD, DBGTXPIN);
-		 ConfigSWM(U0_RXD, DBGRXPIN);
-
-		 // reset de UART0
-		 LPC_SYSCON->PRESETCTRL0 &= (UART0_RST_N);
-		 LPC_SYSCON->PRESETCTRL0 |= ~(UART0_RST_N);
-
-		 // Configuration du générateur de baud rate de USART0
-		 // 7 : 115200 baud
-		 // 15 : 57600 baud
-		 LPC_USART0->BRG = 15;
-
-		 // Configuration du registre USART0 CFG:
-		 // 8 bits de donnees, sans parite/parite paire, un bit de stop, pas de controle de flux, mode asynchrone
-		 LPC_USART0->CFG = DATA_LENG_8|PARITY_EVEN|STOP_BIT_1;
-
-		 // Reset au cas ou une valeur est restee
-		 LPC_USART0->STAT = 0xFFFF;
-
-		 // Activation USART0
-		 LPC_USART0->CFG |= UART_EN;
-
-	//Configuration du CTIMER
-
-		//Reset du timer0
-		LPC_CTIMER0->TCR = (1<<CRST); //Mise à 1 du rst
-		LPC_CTIMER0->TCR &= ~(1<<CRST); //Mise à 0 du rst
-
-		//Pas de prediviseur pour une meilleure precision
-		LPC_CTIMER0->PR = 0;
-
-		//Reglage du modulo du compteur et de l'action réalisée
-		LPC_CTIMER0->MR[3] = (int) 15000000/440;
-		LPC_CTIMER0->MCR |= (1<<MR3R); //faire reset sur la valeur de MR3
-		LPC_CTIMER0->MR[1] = (int) 15000000/(2 * 440);
-
-		//Enable du MSR
-		LPC_CTIMER0->MCR |= (1<<25); //25 est MR1RL
-
-		//Mode PWM
-		LPC_CTIMER0->PWMC |= (1<<PWMEN1);
-
-		//le PWM de la variable TO_MAT0 est assigné à un pin disponible,
-		LPC_SWM->PINASSIGN4 &= ~(0xFF << 8); //Mise a zero pour pouvoir ecrire dans T0_MAT1
-		LPC_SWM->PINASSIGN4 |= 19<<8;
+uint8_t TCS_read_reg(uint8_t reg_ad){
+	uint8_t I2CMasterBuffer[2]; // ad, #reg
+	uint8_t I2CSlaveBuffer[1];
+	uint8_t I2CWriteLength=1;
+	uint8_t I2CReadLength=1;
+	I2CMasterBuffer[0]=TCS34725_ADDRESS;
+	I2CMasterBuffer[1]=(0b100 << 5) | reg_ad;
+	I2CmasterWriteRead( I2CMasterBuffer, I2CSlaveBuffer, I2CWriteLength, I2CReadLength );
+	// autre possibilité :
+//	I2CmasterWrite(I2CMasterBuffer, I2CWriteLength );
+//	I2CmasterRead( MCP23_I2C_AD, I2CSlaveBuffer, I2CReadLength );
+	return I2CSlaveBuffer[0];
+}
 
 
-	//Initialisation de l'afficheur lcd et affichage d'un texte
-	init_lcd();
-	lcd_puts("Appui sur 2 btns");
-	lcd_position(1, 0);
-	lcd_puts("simultanement <>");
+/**
+ * Lecture d'une couleur
+ * color   :   caractere 'r' (rouge), 'v' (vert), 'b' (bleu)
+ */
+uint8_t TCS_read_color(char color){
+	uint8_t reg_ad;
+	uint8_t valeur = 0;
 
-	while (1) {
-		bp1 = BP1;
-		bp2 = BP2;
+	switch (color) {
+		case 'r':
+			reg_ad = 0x16; //low_byte
+			break;
+		case 'v':
+			reg_ad = 0x18;
+			break;
+		case 'b':
+			reg_ad = 0x1A;
+			break;
+		default:
+			erreur_demande = 1;
+			break;
+	}
+	if (reg_ad != 0)
+	{
+		valeur = TCS_read_reg(reg_ad);
+	}
+	return valeur;
+}
 
-		//Lors d'un appui sur les deux boutons simultanement
-			//Le menu change d'etat
-		if(bp1 == enfonce && (bp1_prec != bp1 || bp2_prec != bp2) && bp2 == enfonce)
-		{
-			menu = (menu == 2 || menu == 3) ? 0 : menu + 1;
-		}
+/**
+ * Lecture des couleurs
+ * rouge, vert et bleu   :  pointeur vers ou stocker les couleurs
+ */
+void TCS_read_colors(uint16_t *rouge, uint16_t *vert, uint16_t *bleu)
+{
+	uint8_t reg_ad = 0x16;
 
+	uint8_t I2CMasterBuffer[2]; // ad, #reg
+	uint8_t I2CSlaveBuffer[1];
+	uint8_t I2CWriteLength=1;
+	uint8_t I2CReadLength=6;
+	I2CMasterBuffer[0]=TCS34725_ADDRESS;
+	I2CMasterBuffer[1]=(0b101 << 5) | reg_ad; //Command, auto-increment protocol, adresse du registre
+	I2CmasterWriteRead( I2CMasterBuffer, I2CSlaveBuffer, I2CWriteLength, I2CReadLength );
 
-		switch (menu)
-				{
-					//menu manuel
-					case 0:
-					{
-						if(menu_prec != menu) affichage("Mode manuel", "note jouee : ");
+	//Recuperation des donnees (rgb avec lowbyte puis high byte a chaque fois)
+	*rouge = (I2CSlaveBuffer[1]<<8) | I2CSlaveBuffer[0];
+	*vert = (I2CSlaveBuffer[3]<<8) | I2CSlaveBuffer[2];
+	*bleu = (I2CSlaveBuffer[5]<<8) | I2CSlaveBuffer[4];
+}
 
-						if((LPC_USART0->STAT) & RXRDY)
-								{
-									car_usart = LPC_USART0->RXDAT; //Lecture une seule fois
-									freq = choix_note(car_usart);
-									//Allumage du timer
-									LPC_CTIMER0->TCR = (1<<CEN); //Mise à 1 du rst
-									//Modification du shadow register
-									LPC_CTIMER0->MSR[1] = (int) 15000000/(2 * freq);
-								}
-					}
-						break;
-					//menu auto
-					case 1:
-					{
-						affichage("Mode auto", "musique : ");
-					}
-						break;
-					//menu metronome
-					case 2:
-					{
-						affichage("Mode metronome", "bpm : ");
-					}
-						break;
-					default:
-						break;
-				}
+uint8_t TCS_read(void)
+{
+	uint8_t I2CSlaveBuffer[1];
+	uint8_t I2CReadLength=1;
 
-		bp1_prec = bp1;
-		bp2_prec = bp2;
-		menu_prec = menu;
-
-	} // end of while(1)
-
-} // end of main
+	I2CmasterRead(TCS34725_ADDRESS, I2CSlaveBuffer, I2CReadLength );
+	// autre possibilité :
+//	I2CmasterWrite(I2CMasterBuffer, I2CWriteLength );
+//	I2CmasterRead( MCP23_I2C_AD, I2CSlaveBuffer, I2CReadLength );
+	return I2CSlaveBuffer[0];
+}
 
 /**
  * Affiche du texte sur les deux lignes du lcd
@@ -177,69 +139,61 @@ void affichage(char* texte_l1, char* texte_l2)
 	lcd_puts("                ");
 }
 
-float choix_note(char car)
-{
-	char* note;
-	float frequence;
-	char texte[32];
 
-	switch (car) {
-		case 'a':
-		{
-			note = "DO";
-			frequence = 261.63;
-		}
-			break;
-		case 'z':
-		{
-			note = "RE";
-			frequence = 293.66;
-		}
-			break;
-		case 'e':
-		{
-			note = "MI";
-			frequence = 329.63;
-		}
-			break;
-		case 'r':
-		{
-			note = "FA";
-			frequence = 349.23;
-		}
-			break;
-		case 't':
-		{
-			note = "SOL";
-			frequence = 392;
-		}
-			break;
-		case 'y':
-		{
-			note = "LA";
-			frequence = 440;
-		}
-			break;
-		case 'u':
-		{
-			note = "SI";
-			frequence = 493.88;
-		}
-			break;
-		case 'i':
-		{
-			note = "DO8";
-			frequence = 523.25;
-		}
-			break;
-		default:
-			break;
-	}
-	lcd_position(1, 0);
-	sprintf(texte, "note : %3s", note);
-	lcd_puts(texte);
-	lcd_puts("                ");
-	return frequence;
-}
+int main(void) {
+	//Variables pour les etats des boutons
+	uint8_t bp1 = BP1;
+	uint8_t bp1_prec = BP1;
+	uint8_t bp2 = BP2;
+	uint8_t bp2_prec = BP2;
+
+	//Variable de lecture
+	uint8_t lecture;
+	char texte_lecture[16];
+	char texte_rouge[16];
+
+	//Variable des couleurs lues
+	uint16_t rouge, bleu, vert;
+
+	//Configuration de l'horloge à 15 MHz
+		LPC_PWRD_API->set_fro_frequency(30000);
+
+	// Peripheral reset to the GPIO0 and pin interrupt modules. '0' asserts, '1' deasserts reset.
+		LPC_SYSCON->PRESETCTRL0 &=  (GPIO0_RST_N & GPIOINT_RST_N);
+		LPC_SYSCON->PRESETCTRL0 |= ~(GPIO0_RST_N & GPIOINT_RST_N);
+
+	//Mise en fonctionnement des périphériques utilisés
+		LPC_SYSCON->SYSAHBCLKCTRL0 |= (IOCON | GPIO0 | SWM | CTIMER0 | GPIO_INT);
 
 
+	//Initialisation de l'afficheur lcd et affichage d'un texte
+	init_lcd();
+	lcd_puts("Detecteur colors");
+	TCS_write_reg(0x00, 0b00000011);
+
+	while (1) {
+		bp1 = BP1;
+		bp2 = BP2;
+
+		//Activation du rgb et power on
+
+		//lecture = TCS_read();
+
+		if(bp1==enfonce && bp1_prec != bp1)
+		{
+			lecture = TCS_read_color('r');
+			sprintf(texte_lecture, "val = %x", lecture);
+			affichage("rouge lu", texte_lecture);
+		}
+		if(bp2==enfonce && bp2_prec != bp2)
+		{
+			TCS_read_colors(&rouge, &vert, &bleu);
+			sprintf(texte_rouge, "r%d", rouge);
+			sprintf(texte_lecture, "v%d b%d", vert, bleu);
+			affichage(texte_rouge, texte_lecture);
+		}
+		bp1_prec = bp1;
+		bp2_prec = bp2;
+	} // end of while(1)
+
+} // end of main
